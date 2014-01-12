c4dcc97f01e512efd3c3d8b795b54e237c99a63b
zentol <s.motsu@web.de>
2014-01-13 00:35:10 +0100

    basic game methods

--- MTG/src/Game/Game.java
+++ MTG/src/Game/Game.java
@@ -6,10 +6,14 @@ import Collection.Graveyard;
 import Collection.Hand;
 import Collection.Library;
 import Collection.Stack;
+import Condition.Conditions.ConditionController;
 import Effect.Effect;
 import java.util.ArrayList;
+import static Action.Destroy.Destroy.destroyAll;
+import Condition.Condition;
 
 public class Game {
+    public static ArrayList<Integer> player;
     public static ArrayList<Hand> hand;
     public static ArrayList<Library> library;
     public static ArrayList<Graveyard> graveyard;
@@ -18,11 +22,60 @@ public class Game {
     public static Battlefield battlefield;
     public static Stack stack;
     public static ArrayList<Effect> effects;
-    
-    public static int activePlayer=0;
-    
-    public static void nextTurn(){
+
+    public static int activePlayer = 0;
+
+    public Game(int playerCount) {
+        player = new ArrayList();
+        hand = new ArrayList();
+        library = new ArrayList();
+        graveyard = new ArrayList();
+        exile = new ArrayList();
+        lifepoints = new ArrayList();
+        battlefield = new Battlefield();
+        stack = new Stack();
+        hand = new ArrayList();
+
+        for (int x = 0; x < playerCount; x++) {
+            addPlayer();
+        }
+    }
+
+    public static void turn() {
+        /*
+         supplyPhase();
+         untapPhase();
+         drawPhase();
+         mainPhase();
+         combatPhase();
+         mainPhase();
+         endPhase();
+         */
+    }
+
+    public static void endTurn() {
         activePlayer++;
-        activePlayer%=lifepoints.size();
+        activePlayer %= lifepoints.size();
+    }
+
+    public static void addPlayer() {
+        player.add(player.size());
+        hand.add(new Hand());
+        library.add(new Library());
+        graveyard.add(new Graveyard());
+        exile.add(new Exile());
+        lifepoints.add(20);
+    }
+
+    public static void removePlayer(int playerID) {
+        for (int x = playerID + 1; x < player.size(); x++) {
+            player.set(x, player.get(x) - 1);
+        }
+        hand.remove(playerID);
+        library.remove(playerID);
+        graveyard.remove(playerID);
+        exile.remove(playerID);
+        lifepoints.remove(playerID);
+        destroyAll(new Condition[]{new ConditionController(playerID)});
     }
 }
