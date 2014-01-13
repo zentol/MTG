241f140df65408cb3ba9cd73735a5576e910e896
zentol <s.motsu@web.de>
2014-01-13 23:31:34 +0100

    test: motherfucking indestructible

--- MTG/src/test/test.java
+++ MTG/src/test/test.java
@@ -1,5 +1,6 @@
 package test;
 
+import static Action.Destroy.DestroyAll.destroyAll;
 import Card.Aspect.Permanent.CreatureAspect;
 import Card.Permanent;
 import static Card.Aspect.Permanent.Type.CreatureType.MYR;
@@ -10,12 +11,16 @@ import Condition.Card.ConditionColorPositive;
 import Condition.Card.ConditionSubType;
 import Effect.Effect;
 import Effect.Type.Static.StaticIndestructible;
+import Game.Game;
+import static Game.Game.battlefield;
 import java.util.ArrayList;
 
 public class test {
     public static void main(String[] args) {
-        Permanent c = new Permanent(1, 1, 1, 1, "Footsoldier", "W", "W", false);
+        Game g = new Game(2);
 
+        Permanent c = new Permanent(1, 1, 1, 1, "Footsoldier", "W", "W", false);
+        battlefield.add(c);
         c.addCreatureAspect(1, 1, new String[]{SOLDIER, MYR}, null);
         /*c.addArtifactAspect(new String[]{"Equipment"});
          Creature q = new Creature(1,1,null,null);
@@ -23,11 +28,13 @@ public class test {
         Condition q = new ConditionAspect(CreatureAspect.class);
         Condition p1 = new ConditionColorPositive("W");
         Condition p2 = new ConditionColorPositive("WR");
-        Condition st1 = new ConditionSubType(new String[]{"Soldier",MYR});
+        Condition st1 = new ConditionSubType(new String[]{"Soldier", MYR});
         System.out.println(p1.evaluate(c));
         System.out.println(p2.evaluate(c));
         System.out.println(st1.evaluate(c));
-        c.effects = new ArrayList();
-        c.effects.add(new StaticIndestructible());
+        c.effects.add(new StaticIndestructible(c, c));
+        c.effects.get(0).execute();
+        destroyAll(new Condition[0]);
+        System.out.println(battlefield.size());
     }
 }
