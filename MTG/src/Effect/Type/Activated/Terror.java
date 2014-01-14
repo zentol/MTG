93ec626469809ef738aea28f55c13bb8fdbb881b
zentol <s.motsu@web.de>
2014-01-14 01:17:14 +0100

    Terror effect done

---
+++ MTG/src/Effect/Type/Activated/Terror.java
@@ -0,0 +1,20 @@
+package Effect.Type.Activated;
+
+import static Action.Destroy.DestroyTarget.destroyTarget;
+import Card.Permanent;
+import Card.Spell;
+import Condition.Card.ConditionColorNegative;
+import Condition.Condition;
+import Condition.Permanent.ConditionVulnerableColor;
+
+public class Terror extends Activated{
+    private final Spell source;
+    
+    public Terror(Spell source){
+        this.source=source;
+    }
+    
+    public void execute(Permanent target){
+        destroyTarget(target, new Condition[]{new ConditionColorNegative("B")/*,new ConditionTargetable()*/, new ConditionVulnerableColor(source.colors)});
+    }
+}
