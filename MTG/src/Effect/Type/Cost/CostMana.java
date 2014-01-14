38f754f34a31f0c21bc21083e0480de4e0014716
zentol <s.motsu@web.de>
2014-01-14 21:04:04 +0100

    cost(mana/sacrifice) started

---
+++ MTG/src/Effect/Type/Cost/CostMana.java
@@ -0,0 +1,15 @@
+package Effect.Type.Cost;
+
+import Effect.Effect;
+
+public class CostMana extends Effect {
+    private String cost;
+
+    public CostMana(String cost) {
+        this.cost = cost;
+    }
+    
+    public void pay(String payment){
+        
+    }
+}
