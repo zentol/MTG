71736629ae3a7f5f7dc4c74501e3773020f56397
zentol <s.motsu@web.de>
2014-01-14 01:50:38 +0100

    Aspect: Creature: removed effects

--- MTG/src/Card/Aspect/Permanent/CreatureAspect.java
+++ MTG/src/Card/Aspect/Permanent/CreatureAspect.java
@@ -6,15 +6,12 @@ public class CreatureAspect extends PermanentAspect {
     private final int attack;
     private final int defense;
     private boolean summoningSickness;
-    private final Effect[] effects;
 
     public CreatureAspect(int attack, int defense, String[] types, Effect[] effects) {
         super(types);
-        
         this.summoningSickness = true;
         this.attack = attack;
         this.defense = defense;
-        this.effects = effects;
     }
 
     public void disableSummoningSickness() {
