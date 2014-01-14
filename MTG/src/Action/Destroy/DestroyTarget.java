6e98f50d44efe40cbd99a064647b88377173c3f6
zentol <s.motsu@web.de>
2014-01-14 01:14:33 +0100

    Action: destroy: single/multiple target version

--- MTG/src/Action/Destroy/DestroyTarget.java
+++ MTG/src/Action/Destroy/DestroyTarget.java
@@ -6,15 +6,30 @@ import static Game.Game.battlefield;
 import static Game.Game.graveyard;
 
 public class DestroyTarget {
-    
+
+    /**
+     Destroys target permanents for whom ALL conditions are met.
+     @param conditions conditions to meet
+     */
+    public static void destroyTarget(Permanent permanent, Condition[] conditions) {
+        boolean allConditionsMet = true;
+        for (int y = 0; y < conditions.length; y++) {
+            allConditionsMet &= conditions[y].evaluate(permanent);
+        }
+        if (allConditionsMet) {
+            destroyPermanent(permanent);
+        }
+
+    }
+
     /**
      Destroys every target permanents for whom ALL conditions are met.
      @param conditions conditions to meet
      */
-    public static void destroyTarget(Permanent[] permanents, Condition[] conditions) {
+    public static void destroyTargets(Permanent[] permanents, Condition[] conditions) {
         for (int x = 0; x < permanents.length; x++) {
             boolean allConditionsMet = true;
-            for (int y = 0; x < conditions.length; y++) {
+            for (int y = 0; y < conditions.length; y++) {
                 allConditionsMet &= conditions[y].evaluate(permanents[x]);
             }
             if (allConditionsMet) {
