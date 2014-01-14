0fa756144f32fc492a1a41348c4bbca2afe52dc4
zentol <s.motsu@web.de>
2014-01-14 15:34:11 +0100

    Library: shuffle() implemented

--- MTG/src/Collection/Library.java
+++ MTG/src/Collection/Library.java
@@ -1,6 +1,8 @@
 package Collection;
 
 import Card.Card;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Library extends Staple {
     public Card[] draw(int count) {
@@ -12,6 +14,11 @@ public class Library extends Staple {
     }
 
     public void shuffle() {
+        ArrayList<Card> libraryCopy = (ArrayList<Card>) collection.clone();
+        Random cardPicker = new Random();
+        for (int x = 0; x < collection.size(); x++) {
+            collection.set(x, libraryCopy.remove(cardPicker.nextInt(libraryCopy.size())));
+        }
     }
 
 }
