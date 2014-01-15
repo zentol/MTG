a067dd270cb02189d394107fa4b156317fd775d5
zentol <s.motsu@web.de>
2014-01-15 14:58:45 +0100

    Action: Play: checks for and uses up mana

--- MTG/Action/Action/Play/Play.java
+++ MTG/Action/Action/Play/Play.java
@@ -3,10 +3,15 @@ package Action.Play;
 import Action.Action;
 import Card.Card;
 import Effect.Resolve.Resolve;
+import static Game.Game.activePlayer;
+import static Game.Game.manapool;
 import static Game.Game.stack;
 
 public class Play extends Action {
     public static void play(Card target) {
-        stack.add(new Resolve(target));
+        if (manapool.get(activePlayer).contains(target.cost)) {
+            manapool.get(activePlayer).useMana(target.cost);
+            stack.add(new Resolve(target));
+        }
     }
 }
