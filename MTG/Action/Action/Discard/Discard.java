bb05382072161aceee1b093ccf60ef95a4a02862
zentol <s.motsu@web.de>
2014-01-16 18:06:24 +0100

    Action: Discard: implemented

---
+++ MTG/Action/Action/Discard/Discard.java
@@ -0,0 +1,23 @@
+package Action.Discard;
+
+import Action.Action;
+import Card.Card;
+import static Game.Game.activePlayer;
+import static Game.Game.graveyard;
+import static Game.Game.hand;
+
+public class Discard extends Action {
+    public static void discard(Card[] cards) {
+        for (Card card : cards) {
+            hand.get(activePlayer).remove(card);
+            graveyard.get(activePlayer).sendToGraveyard(card);
+        }
+    }
+    
+    public static void discard(Card[] cards, int playerID) {
+        for (Card card : cards) {
+            hand.get(playerID).remove(card);
+            graveyard.get(playerID).sendToGraveyard(card);
+        }
+    }
+}
