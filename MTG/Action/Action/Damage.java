1fc25f4d5eb12f31742762e19cf9073dbfcb982a
zentol <s.motsu@web.de>
2014-01-24 19:16:55 +0100

    new Action Damage

---
+++ MTG/Action/Action/Damage.java
@@ -0,0 +1,17 @@
+package Action;
+
+import Card.Aspect.Permanent.CreatureAspect;
+import Card.Permanent;
+import static Game.Game.lifepoints;
+import static Game.Game.player;
+
+public class Damage {
+
+    public static void dealDamageToPermanent(int damage, Permanent target) {
+        ((CreatureAspect) target.getAspect(CreatureAspect.class)).dealDamage(damage);
+    }
+
+    public static void dealDamageToPlayer(int damage, int playerID) {
+        lifepoints.set(playerID, lifepoints.get(playerID) - damage);
+    }
+}
