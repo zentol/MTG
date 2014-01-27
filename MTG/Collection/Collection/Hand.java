bd9ad2d06f4d8d7c2abf0ffb6d0cf153f4654b1f
zentol <s.motsu@web.de>
2014-01-27 15:33:37 +0100

    Hand: now has cardLimit field

--- MTG/Collection/Collection/Hand.java
+++ MTG/Collection/Collection/Hand.java
@@ -3,6 +3,8 @@ package Collection;
 import Card.Card;
 
 public class Hand extends Collection<Card> {
+    public int cardLimit = 7;
+
     public Card[] reveal() {
         Card[] revealedCards = new Card[collection.size()];
         for (int x = 0; x < collection.size(); x++) {
