2f009af0f722104f141dc2fc4412e312557afd1d
zentol <s.motsu@web.de>
2014-01-30 16:41:57 +0100

    new Condition: Instance

---
+++ MTG/Condition/Condition/Card/ConditionInstance.java
@@ -0,0 +1,21 @@
+package Condition.Card;
+
+import Card.Card;
+import Condition.Condition;
+
+public class ConditionInstance extends Condition<Card> {
+    private final int[] instanceIDs;
+
+    public ConditionInstance(int[] instanceIDs) {
+        this.instanceIDs = instanceIDs;
+    }
+
+    @Override
+    public boolean evaluate(Card target) {
+        boolean instanceFound = false;
+        for (int instanceID : instanceIDs) {
+            instanceFound |= target.isInstance(instanceID);
+        }
+        return instanceFound;
+    }
+}
