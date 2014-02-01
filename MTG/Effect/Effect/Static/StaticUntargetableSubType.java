d485f9055d596df47e58acde09ac5e4b443e5ba3
zentol <s.motsu@web.de>
2014-02-01 01:05:05 +0100

    new Effect: Untargetable by SubType

---
+++ MTG/Effect/Effect/Static/StaticUntargetableSubType.java
@@ -0,0 +1,26 @@
+package Effect.Static;
+
+import static Action.Protection.addUntargetableColor;
+import Card.Card;
+import Card.Permanent;
+
+public class StaticUntargetableSubType extends StaticEffect {
+    private final String[] subTypes;
+    private final Permanent target;
+
+    public StaticUntargetableSubType(Card source, Permanent target, String[] subTypes) {
+        super(false);
+        this.subTypes = subTypes;
+        this.source = source;
+        this.target = target;
+    }
+
+    @Override
+    public void activate(Card[] targets) {
+    }
+
+    @Override
+    public void execute() {
+        addUntargetableSubType(source, target, subTypes);
+    }
+}
