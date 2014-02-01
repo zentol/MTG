7fe0ef2d09923e5259efb652486fb428463e1255
zentol <s.motsu@web.de>
2014-02-01 22:08:38 +0100

    new Condition: Targeting

---
+++ MTG/Condition/Condition/Effect/ConditionTargeting.java
@@ -0,0 +1,15 @@
+package Condition.Effect;
+
+import Condition.Condition;
+import Effect.Effect;
+
+public class ConditionTargeting extends Condition<Effect> {
+    public ConditionTargeting() {
+    }
+
+    @Override
+    public boolean evaluate(Effect target) {
+        return target.targeting;
+    }
+
+}
